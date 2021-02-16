c30df7ffd8509227aa85b8879dc6a26c7c2b3241
djockovic <djockovic@foi.hr>
2021-02-16 04:31:05 +0100

    Rješenje zadatka za Speck.Academy

---
+++ ProductApp/JavaScript.js
@@ -0,0 +1,24 @@
+﻿window.addEventListener("load", loadEvent);
+
+function loadEvent(){
+    document.getElementById("productInput").addEventListener("click", InputProduct);
+    document.getElementById("productRemove").addEventListener("click", RemoveProducts);
+}
+var productCounter = 0;
+
+
+function InputProduct() {
+    var productTitle = document.getElementById("productTitle").value;
+    document.getElementById("productList").innerHTML += "<button>" + productTitle + "</button>"
+    productCounter++;
+    document.getElementById("productNumber").innerHTML = "<p>Number of products: <span style='color: orangered'>" + productCounter + "</span></p>";
+    document.getElementById("productRemove").innerHTML = "<a href=''>Remove all products</a>";
+    document.getElementById("productTitle").value = "";
+}
+
+function RemoveProducts() {
+    var confirmRemove = confirm("Are you sure that you want to remove all products?");
+    if (!confirmRemove) {
+        event.preventDefault();
+    }
+}
